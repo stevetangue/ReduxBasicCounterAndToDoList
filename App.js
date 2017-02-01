a9cde91079bafcec037d5ab63685dd5739cfe5c9
Steve Tangue <MayTangue@Mac.fritz.box>
2017-02-01 20:42:59 +1000

    Added style and redux beginner

--- App.js
+++ App.js
@@ -1,7 +1,33 @@
+
+require('./styles/style.scss');
+
 import {createStore} from 'redux';
 
-const reducer = ((state=0) => {
+
+// 01. create store
+const reducerStore = ((state=0, action) => {
+	if (action.type === 'BOO') {
+		return state + 1;
+	}
 	return state;
 });
 
-const store = createStore();
+const store = createStore(reducerStore);
+console.log(document.body.innerText);
+
+
+// 02. get the state
+const start = () => {
+	document.body.innerText = store.getState();
+};
+ 
+
+// 03. to change state
+document.addEventListener ('click', ()=>{
+	store.dispatch({type:'BOO'});
+});
+
+// 04 subscribe to change
+store.subscribe(start);
+
+start();
